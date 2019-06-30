9b03bc91657c836b1ebc56f3695d6ebaed776cea
ARJIT JAIN <arjit@cse.iitb.ac.in>
2019-06-30 02:37:36 -0500

    start acgan

---
+++ auxiliary classifier gan/acgan.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+from torchvision import transforms
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as datasets
+import numpy as np
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import torchvision.utils as vutils
+from torch.autograd import Variable
+from torch import LongTensor
+
+transform = transforms.Compose([transforms.Resize(64), transforms.ToTensor()])
+
+fmnist = datasets.FashionMNIST(root="data/", train=True, download=True, transform=transform)
+
+dataloader = torch.utils.data.DataLoader(
+    dataset=fmnist, batch_size=32, shuffle=True, num_workers=1
+)
+
+
+def convTBNReLU(in_channels, out_channels, kernel_size=4, stride=2, padding=1):
+    return nn.Sequential(
+        nn.ConvTranspose2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+        ),
+        nn.BatchNorm2d(out_channels),
+        nn.LeakyReLU(0.2, True),
+    )
+
+
+def convBNReLU(in_channels, out_channels, kernel_size=4, stride=2, padding=1):
+    return nn.Sequential(
+        nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+        ),
+        nn.BatchNorm2d(out_channels),
+        nn.LeakyReLU(0.2, True),
+    )
+
+
+class Generator(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Generator, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.dict = nn.Embedding(10, 100)
+        self.block1 = convTBNReLU(in_channels, 512, 4, 1, 0)
+        self.block2 = convTBNReLU(512, 256)
+        self.block3 = convTBNReLU(256, 128)
+        self.block4 = convTBNReLU(128, 64)
+        self.block5 = nn.ConvTranspose2d(64, out_channels, 4, 2, 1)
+
+    def forward(self, noise, labels):
+        input = torch.mul(self.dict(labels).unsqueeze(-1).unsqueeze(-1), noise)
+        out = self.block1(input)
+        out = self.block2(out)
+        out = self.block3(out)
+        out = self.block4(out)
+        return self.block5(out).tanh()
+
+
+class Discriminator(nn.Module):
+    def __init__(self, in_channels, num_labels):
+        super(Discriminator, self).__init__()
+        self.in_channels = in_channels
+        self.num_labels = num_labels
+        self.block1 = convBNReLU(self.in_channels, 64)
+        self.block2 = convBNReLU(64, 128)
+        self.block3 = convBNReLU(128, 256)
+        self.block4 = convBNReLU(256, 512)
+        self.block5 = nn.Conv2d(512, 64, 4, 1, 0)
+        self.auxiliary = nn.Sequential(nn.Linear(64, self.num_labels), nn.Softmax(dim=1))
+        self.source = nn.Linear(64, 1)
+
+    def forward(self, input):
+        out = self.block1(input)
+        out = self.block2(out)
+        out = self.block3(out)
+        out = self.block4(out)
+        out = self.block5(out)
+        size = out.shape[0]
+        source = torch.sigmoid(self.source(out.view(size, -1)))
+        classes = self.auxiliary(out.view(size, -1))
+        return source, classes
+
+
+criterionSource = nn.BCELoss()
+criterionClass = nn.NLLLoss()
+G = Generator(in_channels=100, out_channels=1).cuda()
+D = Discriminator(in_channels=1, num_labels=10).cuda()
+
+optimizerD = optim.Adam(D.parameters(), lr=0.0002, betas=(0.5, 0.999))
+optimizerG = optim.Adam(G.parameters(), lr=0.0002, betas=(0.5, 0.999))
+
+fixedNoise = torch.randn(32, 100, 1, 1).cuda()
+fixedLabels = LongTensor(np.random.randint(0, 10, 32))
+img_list = []
+GLosses = []
+DLosses = []
+
+for epoch in range(0, 50):
+    print(epoch)
+    for i, data in enumerate(dataloader):
+        trueTensor = torch.Tensor([0.7 + 0.3 * np.random.random()]).cuda()
+        falseTensor = torch.Tensor([0.3 * np.random.random()]).cuda()
+        images, labels = data
+        images, labels = images.cuda(), labels.cuda()
+        D.zero_grad()
+        realSource, realLabels = D(images)
+        realSourceLoss = criterionSource(realSource, trueTensor.expand_as(realSource))
+        realClassLoss = criterionClass(realLabels, labels)
+        realLoss = (realSourceLoss + realClassLoss)/2
+        realLoss.backward()
+        latent = Variable(torch.randn(images.shape[0], 100, 1, 1)).cuda()
+        random_labels = Variable(LongTensor(np.random.randint(0, 10, images.shape[0]))).cuda()
+        fakeData = G(latent, random_labels)
+        fakeSource, fakeLabels = D(fakeData.detach())
+        fakeLoss = (criterionSource(fakeSource, falseTensor.expand_as(fakeSource)) + criterionClass(fakeLabels, random_labels)) /2
+        fakeLoss.backward()
+        lossD = realLoss + fakeLoss
+        optimizerD.step()
+        G.zero_grad()
+        fakeSource, fakeLabels = D(fakeData)
+        lossG = (criterionSource(fakeSource, trueTensor.expand_as(fakeSource)) + criterionClass(fakeLabels, random_labels)) /2
+        lossG.backward()
+        optimizerG.step()
+        GLosses.append(lossG.cpu())
+        DLosses.append(lossD.cpu())
+        print("Loss D {} G {}".format(lossD.cpu(), lossG.cpu()))
+    if (epoch + 1) % 10 == 0:
+        torch.save(G.state_dict(), "G" + str(epoch) + ".pt")
+        torch.save(D.state_dict(), "D" + str(epoch) + ".pt")
+    with torch.no_grad():
+        fake = G(fixedNoise, fixedLabels).detach().cpu()
+    img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
